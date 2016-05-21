4741e67f494791b17dde3558b0914ba5219f47c0
GabrielRF <github@gabrf.com>
2016-05-21 14:21:41 -0400

    File that creates db ok

--- create_db.py
+++ create_db.py
@@ -1,4 +1,24 @@
+import configparser
+import datetime
+import logging
+import logging.handlers
 import sqlite3
 
-conn = sqlite3.connect('kindle.db')
-conn.close()
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.sections()
+    BOT_CONFIG_FILE = 'kindle.conf'
+    config.read(BOT_CONFIG_FILE)
+    log_file = config['DEFAULT']['logfile']
+    db = config['SQLITE3']['data_base']
+
+    LOG_INFO_FILE = log_file
+    logger_info = logging.getLogger('InfoLogger')
+    logger_info.setLevel(logging.DEBUG)
+    handler_info = logging.handlers.RotatingFileHandler(LOG_INFO_FILE,maxBytes=10240,backupCount=5,encoding='utf-8')
+    logger_info.addHandler(handler_info)
+
+    conn = sqlite3.connect(db)
+    conn.close()
+
+    logger_info.info(str(datetime.datetime.now()) + ' Banco de dados criado')
