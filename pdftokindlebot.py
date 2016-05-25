7a71ebe21e93783ea37787c40a90351d38078997
GabrielRF <github@gabrf.com>
2016-05-25 11:45:57 -0400

    Working adding new user and changing email

--- pdftokindlebot.py
+++ pdftokindlebot.py
@@ -2,6 +2,7 @@ import configparser
 import datetime
 import logging
 import logging.handlers
+import telebot
 import sqlite3
 import sys
 
@@ -32,30 +33,34 @@ def upd_user_email(db, table, chatid, destinatario):
     cursor = conn.cursor()
     aux = ('''UPDATE {} SET destinatario = {}
         WHERE chatid = {}''').format(table, destinatario, chatid)
+    # print(aux)
     cursor.execute(aux)
     conn.commit()
     conn.close()
 
 
-def check_user(db, table, chatid):
+def select_user(db, table, chatid, field):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
-    print(db)
-    print(table)
-    aux = ('''SELECT * FROM "{}" WHERE
-        chatid="{}"''').format(table, str(chatid))
-    print(aux)
+    # print(db)
+    # print(table)
+    aux = ('''SELECT {} FROM "{}" WHERE
+        chatid="{}"''').format(field, table, str(chatid))
+    # print(aux)
     cursor.execute(aux)
-    usuarios = cursor.fetchall()
-    print(usuarios)
+    usuarios = cursor.fetchone()
     if usuarios:
-        print('Existe')
+        # print('Existe')
+        data = usuarios
     else:
-        add_user(db, table, chatid, None)
-        print('Nao existe')
+        add_user(db, table, chatid, 'None')
+        # print('Nao existe')
+        data = 'None'
     #for usuarios in cursor.fetchall():
     #    print(str(usuarios))
     conn.close()
+    # print(data)
+    return data
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
@@ -63,9 +68,11 @@ if __name__ == '__main__':
     BOT_CONFIG_FILE = 'kindle.conf'
     config.read(BOT_CONFIG_FILE)
     log_file = config['DEFAULT']['logfile']
+    TOKEN = config['DEFAULT']['TOKEN']
     db = config['SQLITE3']['data_base']
     table = config['SQLITE3']['table']
 
+    bot = telebot.TeleBot(TOKEN)
     LOG_INFO_FILE = log_file
     logger_info = logging.getLogger('InfoLogger')
     logger_info.setLevel(logging.DEBUG)
@@ -73,4 +80,25 @@ if __name__ == '__main__':
         maxBytes=10240, backupCount=5, encoding='utf-8')
     logger_info.addHandler(handler_info)
 
-    check_user(db, table, sys.argv[1])
+    # select_user(db, table, sys.argv[1])
+    @bot.message_handler(commands=['start'])
+    def start(message): 
+        data = select_user(db, table, message.from_user.id, '*')
+        bot.send_message(message.from_user.id, str(data))
+        print(data[3])
+        if data[3] == 'None':
+            msg = bot.send_message(message.from_user.id, 'Type your Kindle e-mail.')
+            bot.register_next_step_handler(msg, ask_email)
+            
+
+    @bot.message_handler(commands=['email'])
+    def ask_email(message): 
+        msg = bot.send_message(message.from_user.id, 'Type your Kindle e-mail.')
+        bot.register_next_step_handler(msg, add_email)
+
+    def add_email(message):
+        upd_user_email(db, table, message.from_user.id, '"' + str(message.text) + '"') 
+        select_user(db, table, message.from_user.id, 'destinatario')
+        bot.reply_to(message, 'Email registered.')
+
+    bot.polling()
