86b4bf343151b7fa8dbd68a24ecb6447dcfca820
Gabriel R F <github@gabrf.com>
2016-05-26 09:46:38 -0300

    First e-mail send test

--- pdftokindlebot.py
+++ pdftokindlebot.py
@@ -1,12 +1,42 @@
 import configparser
 import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
 import logging
 import logging.handlers
+import os
 import requests
+import smtplib
 import sqlite3
 import sys
 import telebot
 from telebot import types
+
+
+def send_mail( send_from, send_to, subject, text, file, server="localhost",
+    port=587, username='', password='', isTls=True):
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime = True)
+    msg['Subject'] = subject
+
+    msg.attach( MIMEText(text) )
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload( open(file,"rb").read() )
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+    msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if isTls: smtp.starttls()
+    # smtp.login(username,password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
 
 
 def add_user(db, table, chatid, destinatario):
@@ -147,7 +177,15 @@ if __name__ == '__main__':
             r = requests.get(file_url)
             bot.send_message(message.from_user.id, 'Downloaded '
                 + str(len(r.content)) + ' bytes.\nSending to Kindle...')
-            
+            send_from = 'grfgabriel@gmail.com'
+            send_to = 'gabrielrf_kindle@kindle.com'
+            subject = 'Convert'
+            text = ''
+            pdf_file = r
+            send_mail( send_from, send_to, subject, text, pdf_file,
+                server="localhost", port=587, username='', password='',
+                isTls=True)
+
 
     @bot.callback_query_handler(lambda q: q.data == '/email')
     def email(call):
