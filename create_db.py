6435149988909fb07aada29910ba6d3f106ad16c
Humberto Rocha <humrochagf@gmail.com>
2020-11-05 09:16:18 -0500

    Add format and linting process to keep the code cleaner

--- create_db.py
+++ create_db.py
@@ -4,21 +4,23 @@ import logging
 import logging.handlers
 import sqlite3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.sections()
-    BOT_CONFIG_FILE = 'kindle.conf'
+    BOT_CONFIG_FILE = "kindle.conf"
     config.read(BOT_CONFIG_FILE)
-    log_file = config['DEFAULT']['logfile']
-    db = config['SQLITE3']['data_base']
+    log_file = config["DEFAULT"]["logfile"]
+    db = config["SQLITE3"]["data_base"]
 
     LOG_INFO_FILE = log_file
-    logger_info = logging.getLogger('InfoLogger')
+    logger_info = logging.getLogger("InfoLogger")
     logger_info.setLevel(logging.DEBUG)
-    handler_info = logging.handlers.RotatingFileHandler(LOG_INFO_FILE,maxBytes=10240,backupCount=5,encoding='utf-8')
+    handler_info = logging.handlers.RotatingFileHandler(
+        LOG_INFO_FILE, maxBytes=10240, backupCount=5, encoding="utf-8"
+    )
     logger_info.addHandler(handler_info)
 
     conn = sqlite3.connect(db)
     conn.close()
 
-    logger_info.info(str(datetime.datetime.now()) + ' Banco de dados criado')
+    logger_info.info(str(datetime.datetime.now()) + " Banco de dados criado")
